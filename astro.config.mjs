cd4dfcbb04e97ed2bb9c9e3113bbdf3b632c5865
itsjustjer <heartdad38@icloud.com>
2024-09-05 03:37:49 -0500

    pita

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
+
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
+ import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify({
-    edgeMiddleware: true
-  }),
+ adapter: netlify()
 });
